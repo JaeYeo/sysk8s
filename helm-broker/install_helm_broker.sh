e98692445c22767b6d6e84c3a5758dcd9b63be73
se.ban@infranicsmail.com <se.ban@infranicsmail.com>
2022-04-20 11:24:04 +0900

    tgz

--- helm-broker/install_helm_broker.sh
+++ helm-broker/install_helm_broker.sh
@@ -5,5 +5,10 @@ helm install helm-broker charts/helm-broker \
  --set global.helm_broker.version=0.5 \
  --set global.helm_controller.dir="rancher/" \
  --set global.helm_controller.version=0.5 \
+ --set webhook.image="registry.systeer.com/rancher/helm-broker-webhook:0.3" \
+ --set etcd-stateful.etcd.image="registry.systeer.com/rancher/etcd" \
+ --set etcd-stateful.etcd.imageTag="v3.3.9" \
+ --set etcd-stateful.tlsSetup.image="registry.systeer.com/rancher/etcd-tls-setup" \
+ --set etcd-stateful.tlsSetup.imageTag="0.3.367" \
  --set imageRegistry="registry.systeer.com"
 
