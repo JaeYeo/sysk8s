dc89bc8caafd1fbf04f9b048fa161293bf0851f1
se.ban@infranicsmail.com <se.ban@infranicsmail.com>
2022-05-03 17:42:45 +0900

    periodSeconds:300

--- postgresql-ha-0.1.0/test.sh
+++ postgresql-ha-0.1.0/test.sh
@@ -1,2 +1,2 @@
 
-helm install postgresql-ha-01 chart/postgresql-ha -f ./plans/micro/values.yaml --namespace postgresql-ha --set postgresql.password='master77!!' --set postgresql.database=sysk8s --set service.port=5433 
+helm install postgresql-ha-01 chart/postgresql-ha -f ./plans/enterprise/values.yaml --namespace postgresql-ha --set postgresql.password='master77!!' --set postgresql.database=sysk8s --set service.port=5433 
