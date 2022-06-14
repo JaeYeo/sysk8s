7e5c31488632b191ee4bfe266a95f1aaa2c374d6
se.ban@infranicsmail.com <se.ban@infranicsmail.com>
2022-06-14 15:13:39 +0900

    helm kong addons

--- kong-0.1.1/test.sh
+++ kong-0.1.1/test.sh
@@ -1,2 +1,2 @@
 
-helm install kong-02 chart/kong --namespace kong2 -f ./plans/micro/values.yaml --set fullnameOverride=kong-02
+helm install kong-02 chart/kong --namespace kong2 -f ./values.yaml --set fullnameOverride=kong-02
